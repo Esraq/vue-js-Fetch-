3bc5819246860dad9f942af5d202f0bc991d6014
Esraq <esraq26552@gmail.com>
2020-11-16 15:53:48 +0600

    fetch

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Fetch data with API</h1>
+    <EmployeeList/>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+import EmployeeList from './components/EmployeeList'
+
+export default {
+  name: 'App',
+  components: {
+    EmployeeList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,72 @@
+<template>
+    
+    <div id="app">
+
+
+     <h1>Employee List</h1>
+
+       <table border="1px">
+         <tr>
+             
+             <td>Name</td>
+             <td>Salary</td>
+             <td>Age</td>
+
+      
+          </tr>   
+
+        <tr  v-for="item in list" v-bind:key="item.id">
+
+
+                  <td>{{item.id}}</td>
+             <td>{{item.name}}</td>
+             <td>{{item.detail}}</td>
+
+
+
+            </tr> 
+
+
+        </table>   
+
+     
+
+
+
+        </div>
+
+
+</template>
+
+<script>
+
+import Vue from 'vue';
+import VueAxios from 'vue-axios'
+import axios from 'axios';
+Vue.use(VueAxios,axios)
+
+export default {
+    
+  name:"EmployeeList",
+  data(){
+   
+
+      return{
+
+          list:undefined
+      }
+
+  },
+
+  mounted()
+  {
+
+      Vue.axios.get('http://127.0.0.1:8000/api/products')
+      .then(resp=>{
+          this.list=resp.data.data
+          console.warn(resp.data.data)
+      })
+  }
+
+}
+</script>
